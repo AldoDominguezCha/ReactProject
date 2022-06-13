27f9899db6641ffc30f374a3d0d48b53c989a6ec
Aldo Dominguez <aldodominguezchz@gmail.com>
2022-06-12 20:26:10 -0500

    add logic for cart animation when adding/removing items

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 
 import CartIcon from './../Cart/CartIcon';
 import CartContext from './../../store/cart-context';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = props => {
-
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
     const numberOfCarItems = cartCtx.items.reduce((acc, current) => acc + current.amount, 0);
 
@@ -15,8 +15,22 @@ const HeaderCartButton = props => {
         props.onClick(true);
     }
 
+    const btnClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+    useEffect(() => {
+        if (cartCtx.items.length === 0) return;
+        setBtnIsHighlighted(true);
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        }
+    }, [cartCtx.items]);
+
     return (
-        <button onClick={cartButtonClickHandler} className={styles.button}>
+        <button onClick={cartButtonClickHandler} className={btnClasses}>
             <span className={styles.icon}><CartIcon/></span>   
             <span>Your cart</span>   
             <span className={styles.badge}>
